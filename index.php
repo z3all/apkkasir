0c14a59b2d679dd7d4e62f201e415ea13d5e65de
z3all <yfendi925@gmail.com>
2025-01-20 07:56:57 +0700

    ubah footer

--- index.php
+++ index.php
@@ -87,18 +87,7 @@
                         include($page . '.php');
                     ?>
                 </main>
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid px-4">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Aplikasi Kasir 2025</div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
+                
             </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
